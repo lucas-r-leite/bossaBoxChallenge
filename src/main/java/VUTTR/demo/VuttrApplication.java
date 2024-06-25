271b5a1e39bcc22a3dad41bab3f707cce2d92860
lucas <lucasgit.breeding934@passinbox.com>
2024-06-25 14:02:15 -0300

    Spring Boot started

---
+++ src/main/java/VUTTR/demo/VuttrApplication.java
@@ -0,0 +1,13 @@
+package VUTTR.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuttrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuttrApplication.class, args);
+	}
+
+}
