1e8dc364f8f46c0036361ca65547d69154b65277
lucas <lucasgit.breeding934@passinbox.com>
2024-06-25 15:51:59 -0300

    PUT added at controller

--- src/main/java/VUTTR/demo/controller/ToolsController.java
+++ src/main/java/VUTTR/demo/controller/ToolsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,6 +42,16 @@ public ResponseEntity<List<ToolsDTO>> findAll(@RequestParam(required = false) St
         try{
             Tools tools = toolsService.save(body);
             return new ResponseEntity<>(tools, HttpStatusCode.valueOf(201));
+        }catch(Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatusCode.valueOf(400));
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateTool(@PathVariable Long id, @RequestBody Tools body){
+        try{
+            Tools tools = toolsService.updateTools(body);
+            return new ResponseEntity<>(tools, HttpStatusCode.valueOf(200));
         }catch(Exception e){
             return new ResponseEntity<>(e.getMessage(), HttpStatusCode.valueOf(400));
         }
